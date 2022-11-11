ea2177da90b5ce35a4c2b2c2500aa8caf6df9e0c
jihadeid <115483981+jihadeid@users.noreply.github.com>
2022-11-11 17:54:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   
-   cout<<"Hello, World!"<<endl;
-   return 0;
+int main(){
+    //variabili
+    float a,b,c;
+
+    //input
+    cin >>a>>b>>c;
+
+    //area triangolo
+    cout <<0.5*a*b<<endl;
+
+    //area quadrato
+    cout <<a*a<<endl;
+
+    //area rettangolo
+    cout << a*b<<endl;
+
+    //area trapezio
+    cout << 0.5*(a+b)*c<<endl;
+
+    return 0;
+
 }
